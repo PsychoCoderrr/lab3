7de04a0dc2f203d097f2a6edcd6d17b2db9e06d1
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-05-03 00:40:13 +0300

    moved Sequence and DynamicArray classes, added constructors and Get-functions to the Vector class

--- main.cpp
+++ main.cpp
@@ -164,7 +164,47 @@ template <typename T> class Vector
 private:
     DynamicArray<T> elements;
 public:
+    Vector (){this->elements = new DynamicArray<T>();}
     
+    Vector (int count) {this->elements = new DynamicArray<T>(count);}
+    
+    Vector (T *items, int count)
+    {
+      this->elements = new DynamicArray<T> (items, count);
+    }
+    
+    Vector (Sequence<T> &seq)
+    {
+      this->elements = new DynamicArray<T> (seq.GetLength ());
+      for (int i = 0; i < seq.GetLength (); i++)
+        {
+          this->elements->Set (seq.Get (i), i);
+            this->elements->Resize (this->GetLength () - 1);
+        }
+    }
+    
+    Vector (Vector<T> &vec)
+    {
+      this->elements = new DynamicArray<T> (vec.GetLength ());
+      for (int i = 0; i < vec.GetLength (); i++)
+        {
+          this->elements->Set (vec.Get (i), i);
+          this->elements->Resize (this->GetLength () - 1);
+        }
+    }
+    
+    T GetFirst()
+    {
+        return this->elements->Get(0);
+    }
+    T GetLast()
+    {
+        return this->elements->Get(this->array->GetSize() - 1);
+    }
+    T Get (int index) const override
+    {
+        return this->elements->Get(index);
+    }
 };
 
 int main(int argc, const char * argv[]) {
