1fb1b3e11f7c2ddd1373b3b67753a7eea1c8dfde
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-05-08 13:31:09 +0300

    added func to print ui

--- main.cpp
+++ main.cpp
@@ -5,39 +5,14 @@
 #include "Complex.h"
 #include "tests.h"
 #include "Person.h"
+#include "UiPrint.h"
 
 int main(int argc, const char *argv[])
 {
     StandartTests();
     TestForComplex();
     int status = 0;
-    std::cout << "0. Run tests for Vector\n";
-    std::cout << "1. Run tests for Stack\n";
-    std::cout << "2. Run tests for Queue\n";
-    std::cout << "3. Use Sum of Vectors for int Vectors\n";
-    std::cout << "4. Use Multi of Vectors for int Vectors\n";
-    std::cout << "5. Use Multi on scalar For int Vector\n";
-    std::cout << "6. Use Sum of Vectors for complex Vectors\n";
-    std::cout << "7. Use Multi of Vectors for complex Vectors\n";
-    std::cout << "8. Use Concat for Stack of int\n";
-    std::cout << "9. Use SubStack for Stack of int\n";
-    std::cout << "10. Use IsSuqSequenceHere for Stack of int\n";
-    std::cout << "11. Use Concat for Stack of complex\n";
-    std::cout << "12. Use SubStack for Stack of complex\n";
-    std::cout << "13. Use IsSuqSequenceHere for Stack of complex\n";
-    std::cout << "14. Use Concat for Stack of Person\n";
-    std::cout << "15. Use SubStack for Stack of Person\n";
-    std::cout << "16. Use IsSuqSequenceHere for Stack of Person\n";
-    std::cout << "17. Use Concat for Queue of int\n";
-    std::cout << "18. Use SubStack for Queue of int\n";
-    std::cout << "19. Use IsSuqSequenceHere for Queue of int\n";
-    std::cout << "20. Use Concat for Queue of complex\n";
-    std::cout << "21. Use SubStack for Queue of complex\n";
-    std::cout << "22. Use IsSuqSequenceHere for Queue of complex\n";
-    std::cout << "23. Use Concat for Queue of Person\n";
-    std::cout << "24. Use SubStack for Queue of Person\n";
-    std::cout << "25. Use IsSuqSequenceHere for Queue of Person\n";
-    std::cout << "26. Stop programm\n";
+    PrintUI();
     int flag = 1;
     while (flag)
     {
@@ -753,33 +728,7 @@ int main(int argc, const char *argv[])
         default:
             std::cout << "Неизвестная команда\n";
         }
-        std::cout << "0. Run tests for Vector\n";
-        std::cout << "1. Run tests for Stack\n";
-        std::cout << "2. Run tests for Queue\n";
-        std::cout << "3. Use Sum of Vectors for int Vectors\n";
-        std::cout << "4. Use Multi of Vectors for int Vectors\n";
-        std::cout << "5. Use Multi on scalar For int Vector\n";
-        std::cout << "6. Use Sum of Vectors for complex Vectors\n";
-        std::cout << "7. Use Multi of Vectors for complex Vectors\n";
-        std::cout << "8. Use Concat for Stack of int\n";
-        std::cout << "9. Use SubStack for Stack of int\n";
-        std::cout << "10. Use IsSuqSequenceHere for Stack of int\n";
-        std::cout << "11. Use Concat for Stack of complex\n";
-        std::cout << "12. Use SubStack for Stack of complex\n";
-        std::cout << "13. Use IsSuqSequenceHere for Stack of complex\n";
-        std::cout << "14. Use Concat for Stack of Person\n";
-        std::cout << "15. Use SubStack for Stack of Person\n";
-        std::cout << "16. Use IsSuqSequenceHere for Stack of Person\n";
-        std::cout << "17. Use Concat for Queue of int\n";
-        std::cout << "18. Use SubStack for Queue of int\n";
-        std::cout << "19. Use IsSuqSequenceHere for Queue of int\n";
-        std::cout << "20. Use Concat for Queue of complex\n";
-        std::cout << "21. Use SubStack for Queue of complex\n";
-        std::cout << "22. Use IsSuqSequenceHere for Queue of complex\n";
-        std::cout << "23. Use Concat for Queue of Person\n";
-        std::cout << "24. Use SubStack for Queue of Person\n";
-        std::cout << "25. Use IsSuqSequenceHere for Queue of Person\n";
-        std::cout << "26. Stop programm\n";
+        PrintUI();
     }
     return 0;
 }
