40610975f65948efea2a1ab378bc2eea9c1c7aba
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-05-15 15:23:22 +0300

    deleted comments and changed uiPrint

--- UiPrint.h
+++ UiPrint.h
@@ -3,31 +3,16 @@
 
 void PrintUI()
 {
-    std::cout << "0. Run tests for Vector\n";
-    std::cout << "1. Run tests for Stack\n";
-    std::cout << "2. Run tests for Queue\n";
-    std::cout << "3. Use Sum of Vectors for int Vectors\n";
-    std::cout << "4. Use Multi of Vectors for int Vectors\n";
-    std::cout << "5. Use Multi on scalar For int Vector\n";
-    std::cout << "6. Use Sum of Vectors for complex Vectors\n";
-    std::cout << "7. Use Multi of Vectors for complex Vectors\n";
-    std::cout << "8. Use Concat for Stack of int\n";
-    std::cout << "9. Use SubStack for Stack of int\n";
-    std::cout << "10. Use IsSuqSequenceHere for Stack of int\n";
-    std::cout << "11. Use Concat for Stack of complex\n";
-    std::cout << "12. Use SubStack for Stack of complex\n";
-    std::cout << "13. Use IsSuqSequenceHere for Stack of complex\n";
-    std::cout << "14. Use Concat for Stack of Person\n";
-    std::cout << "15. Use SubStack for Stack of Person\n";
-    std::cout << "16. Use IsSuqSequenceHere for Stack of Person\n";
-    std::cout << "17. Use Concat for Queue of int\n";
-    std::cout << "18. Use SubStack for Queue of int\n";
-    std::cout << "19. Use IsSuqSequenceHere for Queue of int\n";
-    std::cout << "20. Use Concat for Queue of complex\n";
-    std::cout << "21. Use SubStack for Queue of complex\n";
-    std::cout << "22. Use IsSuqSequenceHere for Queue of complex\n";
-    std::cout << "23. Use Concat for Queue of Person\n";
-    std::cout << "24. Use SubStack for Queue of Person\n";
-    std::cout << "25. Use IsSuqSequenceHere for Queue of Person\n";
-    std::cout << "26. Stop programm\n";
+    std::cout<<std::endl;
+    std::cout << "Vector types: int, complex" << std::endl;
+    std::cout << "Stack types: int, complex, person" << std::endl;
+    std::cout << "Queue types: int, complex, person" << std::endl;
+    std::cout << "Vector functions: sum, multi, multionscalar"<< std::endl;
+    std::cout << "Stack functions: concat, substack, issubsequencehere" << std::endl;
+    std::cout << "Queue functions: concat, subqueue, issubsequencehere" << std::endl;
+    std::cout << "Comands you can enter:" << std::endl;
+    std::cout << "Run tests for <name of container>" << std::endl;
+    std::cout << "Use <type> <name of container> for <function>" << std::endl;
+    std::cout << "Stop" << std::endl << std::endl;
+    
 }
